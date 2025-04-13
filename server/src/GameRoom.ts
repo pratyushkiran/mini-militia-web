060ddf3bb09a29176ec5728d4efd5eb997e82cac
Pratyush <pratyush03102001@gmail.com>
2025-04-14 04:42:46 +0530

    Day 4: Add join/leave broadcasts and room locking

--- server/src/GameRoom.ts
+++ server/src/GameRoom.ts
@@ -13,24 +13,38 @@ class GameState extends Schema {
 
 export class GameRoom extends Room<GameState> {
   maxClients = 4;
+  minClients = 2; // For testing
 
   onCreate(options: any) {
     console.log("GameRoom created:", options);
     this.setState(new GameState());
     this.autoDispose = true;
+    // Lock room when full
+    this.onMessage("ready", (client) => {
+      if (this.clients.length >= this.minClients) {
+        this.lock();
+        console.log("Room locked with", this.clients.length, "players");
+      }
+    });
   }
 
   async onAuth(client: Client, options: any) {
-    return true; // Open for now; Firebase on Day 7
+    return true;
   }
 
   onJoin(client: Client, options: any) {
     const player = new Player();
     player.sessionId = client.sessionId;
-    player.x = 100 + this.state.players.size * 50; // Stagger spawns
+    player.x = 100 + this.state.players.size * 50;
     this.state.players.set(client.sessionId, player);
     console.log(
       `${client.sessionId} joined. Players: ${this.state.players.size}`
+    );
+    // Broadcast join
+    this.broadcast(
+      "player_joined",
+      { sessionId: client.sessionId },
+      { except: client }
     );
   }
 
@@ -40,6 +54,12 @@ export class GameRoom extends Room<GameState> {
       console.log(
         `${client.sessionId} left. Players: ${this.state.players.size}`
       );
+      this.broadcast("player_left", { sessionId: client.sessionId });
+      // Unlock if below minClients
+      if (this.clients.length < this.minClients && this.locked) {
+        this.unlock();
+        console.log("Room unlocked");
+      }
     }
   }
 
